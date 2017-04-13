6d7516976001e28837c06cf5c4972a6fbac5d764
Diana Hernández Soler <dianahdezsoler@gmail.com>
2017-04-13 21:39:55 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
-var canvas =  document.getElementById('canvas_game');
-var contexto = canvas.getContext('2d');
-
+	var posx=0;
+	var posy=0;
+	function dibujapunto(despx,despy){
+		posx=posx+despx;
+		posy=posy+despy;
+		var canvas=document.getElementById('canvas_game');
+		var context=canvas.getContext('2d');
+		context.fillStyle='red';
+		context.fillRect(posx,posy,5,5);
+	}
 
 
